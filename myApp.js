cc40526c5ea2be66665fa08cb9135790a45a3985
Blances Sanchez <blancessanchez30@gmail.com>
2021-08-15 22:29:24 +0800

    Learned working on Express JS and serving static route

--- myApp.js
+++ myApp.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
-
-
-
+console.log('Hello World');
+app.use('/public', express.static(__dirname + '/public/'));
+app.get('/', (req, res) => {
+  // res.send('Hello Express');
+  res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/json', (req, res) => {
+  res.json({
+    message: "Hello json"
+  })
+});
 
 
 
