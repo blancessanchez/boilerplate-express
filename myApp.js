bea508e8a578f490791ad2a9d4481d3b853ea65c
Blances Sanchez <blancessanchez30@gmail.com>
2021-08-16 23:44:38 +0800

    Removed comment

--- myApp.js
+++ myApp.js
@@ -3,7 +3,6 @@ var app = express();
 console.log('Hello World');
 app.use('/public', express.static(__dirname + '/public/'));
 app.get('/', (req, res) => {
-  // res.send('Hello Express');
   res.sendFile(__dirname + '/views/index.html');
 });
 app.get('/json', (req, res) => {
