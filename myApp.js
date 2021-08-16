190ff557880f0a2fd1da615b25a21ddd34430a58
Blances Sanchez <blancessanchez30@gmail.com>
2021-08-17 00:50:40 +0800

    Learned about env and root-level middleware

--- myApp.js
+++ myApp.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var app = express();
+
+// meet the node console
 console.log('Hello World');
+
+// serve static assets
 app.use('/public', express.static(__dirname + '/public/'));
+
+// serve an HTML file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
-app.get('/json', (req, res) => {
-  res.json({
-    message: "Hello json"
-  })
-});
 
+// serve JSON on a specific route
+// app.get('/json', (req, res) => {
+//   res.json({
+//     message: "Hello json"
+//   })
+// });
 
+// use .env file
+// app.get('/json', (req, res) => {
+//   process.env.MESSAGE_STYLE === 'uppercase' ? 
+//     res.json({ "message": "HELLO JSON" }):
+//     res.json({ "message": "Hello json" })
+// });
+
+// root-level request logger middleware
+// app.use(function(req, res, next) {
+//   console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+//   next();
+// });
 
 
 
